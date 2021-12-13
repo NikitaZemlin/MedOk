4843d38fce57b24608174ccf938e7e582bc965d5
Zemlin Nikita <Darnik142@gmail.com>
2021-12-13 17:07:44 +0300

    Регистрация и вход работают

--- app/src/main/java/com/example/medok/MainActivity.java
+++ app/src/main/java/com/example/medok/MainActivity.java
@@ -1,14 +1,79 @@
 package com.example.medok;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    EditText ETLogin, ETPassword;
+    Button BTNLogin;
+    TextView TVNoAccount;
+    FirebaseAuth fAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ETLogin = findViewById(R.id.ETLogin);
+        ETPassword = findViewById(R.id.ETPassword);
+        BTNLogin = findViewById(R.id.BTNLogin);
+        BTNLogin.setOnClickListener(this);
+        TVNoAccount = findViewById(R.id.TVNoAccount);
+        TVNoAccount.setOnClickListener(this);
+
+        fAuth = FirebaseAuth.getInstance();
+
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.BTNLogin:
+                String login = ETLogin.getText().toString();
+                String password = ETPassword.getText().toString();
+
+                if (TextUtils.isEmpty(login)){
+                    ETLogin.setError("Некорректный логин");
+                    return;
+                }
+                if (TextUtils.isEmpty(password)){
+                    ETPassword.setError("Некорретный пароль");
+                }
+
+                fAuth.signInWithEmailAndPassword(login, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            Toast.makeText(MainActivity.this, "Вход выполнен!", Toast.LENGTH_SHORT).show();
+                        }
+                        else{
+                            Toast.makeText(MainActivity.this, "Ошибка!", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+
+                break;
+            case R.id.TVNoAccount:
+                startActivity(new Intent(MainActivity.this, RegisterActivity.class));
+                break;
+        }
+
     }
 }
---
+++ app/src/main/java/com/example/medok/RegisterActivity.java
@@ -0,0 +1,73 @@
+package com.example.medok;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class RegisterActivity extends AppCompatActivity implements View.OnClickListener {
+    EditText ETLoginReg, ETPasswordReg;
+    Button BTNLoginReg;
+    TextView TVYesAccount;
+    FirebaseAuth fAuth;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+
+        ETLoginReg = findViewById(R.id.ETLoginReg);
+        ETPasswordReg = findViewById(R.id.ETPasswordReg);
+        BTNLoginReg = findViewById(R.id.BTNLoginReg);
+        BTNLoginReg.setOnClickListener(this);
+        TVYesAccount = findViewById(R.id.TVYesAccount);
+        TVYesAccount.setOnClickListener(this);
+
+        fAuth = FirebaseAuth.getInstance();
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.BTNLoginReg:
+                String login = ETLoginReg.getText().toString();
+                String password = ETPasswordReg.getText().toString();
+
+                if (TextUtils.isEmpty(login)){
+                    ETLoginReg.setError("Почта уже занят!");
+                    return;
+                }
+
+                fAuth.createUserWithEmailAndPassword(login, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            Toast.makeText(RegisterActivity.this, "Пользователь создан!", Toast.LENGTH_SHORT).show();
+                        }
+                        else{
+                            Toast.makeText(RegisterActivity.this, "Ошибка!", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+                break;
+
+
+
+            case R.id.TVYesAccount:
+                startActivity(new Intent(RegisterActivity.this, MainActivity.class));
+                break;
+        }
+
+    }
+}
